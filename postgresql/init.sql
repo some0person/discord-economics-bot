ceb0d0213d91bce3c57f576f432f62ab6f5c6792
some0person <109872677+some0person@users.noreply.github.com>
2023-02-24 00:08:44 +0300

    Now it launches in docker with postgresql database

---
+++ postgresql/init.sql
@@ -0,0 +1,14 @@
+CREATE TABLE settings (
+    server_id INTEGER UNIQUE NOT NULL,
+    prefix VARCHAR(1) NOT NULL,
+    reaction VARCHAR NOT NULL,
+    l_channels VARCHAR,
+    s_channel INTEGER,
+    r_cost INTEGER NOT NULL,
+    award INTEGER NOT NULL);
+
+
+CREATE TABLE data (
+    server_id INTEGER NOT NULL,
+    user_id INTEGER NOT NULL,
+    score INTEGER NOT NULL)
